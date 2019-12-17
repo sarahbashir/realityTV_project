a36891ad9dae57ffa802b49ca9e813fd303fb369
Ethan Ong <hobbs8282@gmail.com>
2019-12-16 23:39:02 -0800

    visualization 1

--- Shiny/server.R
+++ Shiny/server.R
@@ -10,7 +10,33 @@ mydata <- reactive({
   output$table <- renderTable({
     mydata()
   })
-  
+  # Define server logic for slider examples ----
+  server <- function(input, output) {
+    
+    # Reactive expression to create data frame of all input values ----
+    sliderValues <- reactive({
+      
+      data.frame(
+        Name = c("Integer",
+                 "Decimal",
+                 "Range",
+                 "Custom Format",
+                 "Animation"),
+        Value = as.character(c(input$integer,
+                               input$decimal,
+                               paste(input$range, collapse = " "),
+                               input$format,
+                               input$animation)),
+        stringsAsFactors = FALSE)
+      
+    })
+    
+    # Show the values in an HTML table ----
+    output$values <- renderTable({
+      sliderValues()
+    })
+    
+  }  
   
   
   
